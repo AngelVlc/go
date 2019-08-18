96acd871f00c2269fd36bf23d0d9a22c5f6ca17d
Angel Ballesteros <angel.ballesteros@flywire.com>
2019-08-18 22:49:17 +0200

    Cli first version

---
+++ workinghours_cli/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/AngelVlc/go/workingmonth"
+)
+
+func main() {
+	var year, month int
+
+	fmt.Print("Year: ")
+	if _, err:= fmt.Scan(&year); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Month: ")
+	if _, err:= fmt.Scan(&month); err != nil {
+		log.Fatal(err)
+	}
+
+	wm := workingmonth.WorkingMonth{
+		Year: year, 
+		Month: month,
+	}
+	fmt.Printf("Working hours for %v/%02d: %v\n", wm.Year, wm.Month, wm.WorkingHours())
+}
